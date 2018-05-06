49444ca79001172561a8e0ad1383316583e05a6e
rasyid11 <sabakunorasyid@gmail.com>
2018-05-06 22:00:03 +0800

    Mengisi nilai atribute

--- src/praktek02/VolumeAksi.java
+++ src/praktek02/VolumeAksi.java
@@ -3,5 +3,8 @@ package praktek02;
 public class VolumeAksi {
     public static void main(String[] args) {
         Volume v1 = new Volume();
+        v1.panjang = 7;
+        v1.lebar = 5;
+        v1.tinggi = 4;
     }
 }
