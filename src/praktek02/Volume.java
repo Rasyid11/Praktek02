9c83a6dfea3d34715b35fda9a04470146e400de8
rasyid11 <sabakunorasyid@gmail.com>
2018-05-06 21:46:14 +0800

    Membuat Attribute dan Behavior

--- src/praktek02/Volume.java
+++ src/praktek02/Volume.java
@@ -1,4 +1,14 @@
 package praktek02;
 public class Volume {
+    double panjang;
+    double lebar;
+    double tinggi;
     
+    void cetakInfo(){
+        System.out.println("==================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar :   "+lebar);
+        System.out.println("Lebar :   "+tinggi);
+        System.out.println("==================");
+    }
 }
